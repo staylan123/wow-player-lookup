e58c3d3100730fbc850584a9076d170b6589b973
Sinan <staylan123@gmail.com>
2025-05-26 14:05:40 -0400

    add images + constants

---
+++ src/app/constants/colors.ts
@@ -0,0 +1,15 @@
+export const ClassColors: Record<string, string> = {
+  "Death Knight": "#C41F3B",
+  "Demon Hunter": "#A330C9",
+  "Druid": "#FF7C0A",
+  "Evoker": "#33937F",
+  "Hunter": "#AAD372",
+  "Mage": "#3FC7EB",
+  "Monk": "#00FF98",
+  "Paladin": "#F58CBA",
+  "Priest": "#FFFFFF",
+  "Rogue": "#FFF468",
+  "Shaman": "#0070DD",
+  "Warlock": "#8788EE",
+  "Warrior": "#C69B6D"
+};
