3920884e1102fde82624010237d41287187fb005
Sinan <staylan123@gmail.com>
2025-05-27 19:33:58 -0400

    fix some types

--- src/app/utils/profile.utils.ts
+++ src/app/utils/profile.utils.ts
@@ -1,5 +1,5 @@
 import { PlayerGearSlotLabels } from "../constants/gear";
-import { Profile } from "../types/types";
+import { Gear, Profile } from "../types/types";
 
 export type PlayerGearData = {
   item_level: number,
@@ -12,19 +12,20 @@ export type PlayerGearCollection= { [key: string] : PlayerGearData }
 
 export const extractPlayerGear = (profile: Profile) : PlayerGearCollection => {
   const { gear } = profile;
-  const items = gear.items as any;
-  const playerGear = {} as any;
+  const items = gear.items;
+  const playerGear : Partial<PlayerGearCollection> = {};
 
   for (const slot in items) {
-    const gear = items[slot];
+    const key = slot as keyof typeof items
+    const gear = items[key];
 
-    playerGear[slot] = {
+    playerGear[key] = {
       item_level: gear.item_level,
       item_quality: gear.item_quality,
       name: gear.name,
-      slotLabel: PlayerGearSlotLabels[slot]
+      slotLabel: PlayerGearSlotLabels[key]
     };
   }
 
-  return playerGear;
+  return playerGear as PlayerGearCollection;
 };
